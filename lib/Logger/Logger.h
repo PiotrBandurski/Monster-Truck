27c12ae7eb15f55229a7bc78124945a31fe5ceb9
Piotr Bandurski <piotr.bandurski@valueadd.pl>
2020-10-24 16:52:41 +0200

    asd

--- lib/Logger/Logger.h
+++ lib/Logger/Logger.h
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
 
 
 void logD(String message);
+void logD(unsigned long n, uint8_t base);
+
+void setLoggingClient(AsyncWebSocketClient * client);
--- lib/SteeringListener/SteeringListener.h
+++ lib/SteeringListener/SteeringListener.h
@@ -1,4 +1,3 @@
 
-
 void startListening();
 void attachVariablesToListen(int* motur, int* kiera, bool* handleOta);
